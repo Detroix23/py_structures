4b4093fda42e0c3e70e013b13c72c9feaa1ac6d9
Detroix23 <detroix23@gmail.com>
2026-02-07 11:08:23 +0100

    Init imports.

--- src/structures_detroix23/__init__.py
+++ src/structures_detroix23/__init__.py
@@ -1,4 +1,15 @@
+# type: ignore
 """
 # Data structures.
 /src/structures_detroix23/__init__.py
 """
+
+from structures_detroix23.modules import (
+	base,
+	cell,
+	dynamic_list,
+	graphs,
+	nodes,
+	result,
+	types,
+)
